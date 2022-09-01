0054435dff6a1f6f57650f63851a0011ac718ddd
Matthew Kilgore <m.kilgore0@gmail.com>
2022-09-01 12:48:25 -0400

    Implemented (very) basic animations and prerender step.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Configs
 const EDGE = 30, RISE = 26, RUN = 15
 
+// Timing
+let currTime = 0, prevTime = 0
+
 // Canvas management
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
@@ -79,6 +82,7 @@ class Hexagon {
     render() {    
         let absX = this.x + (this.parent?.absX ?? 0)
         let absY = this.y + (this.parent?.absY ?? 0)
+        ctx.globalAlpha = 1
         ctx.fillStyle = this.fill
         ctx.strokeStyle = "#33FF00"
         ctx.beginPath()
@@ -108,12 +112,14 @@ class TextSprite {
         this.fill = fill
 
         this.parent = null
+        this.alpha = 1
     }
 
     render() {
         let absX = this.x + (this.parent?.absX ?? 0)
         let absY = this.y + (this.parent?.absY ?? 0)
 
+        ctx.globalAlpha = this.alpha
         ctx.fillStyle = this.fill
         ctx.textBaseline = "middle"
         ctx.textAlign = "center"
@@ -121,9 +127,13 @@ class TextSprite {
         ctx.font = `${getCorrectSize(1.8 * RISE)}px serif`
 
         ctx.fillText(this.text, absX, absY)
+        ctx.alpha = 1
+
+        this.alpha = 1
     }
 }
 
+// Hexagonal coordinates
 class HexGrid {
     constructor(widthRows, heightCols, x, y, parity = 0) {
         this.numRows = widthRows
@@ -170,9 +180,15 @@ class HexGrid {
         let j = Math.floor(col / 2)
 
         this.rows[i][j] = sprite
-        sprite.x = col * (EDGE + RUN)
-        sprite.y = row * RISE
-        sprite.parent = this
+
+        if (sprite !== null) {
+            sprite.x = col * (EDGE + RUN)
+            sprite.y = row * RISE
+            sprite.parent = this
+
+            if ("row" in sprite) sprite.row = row
+            if ("col" in sprite) sprite.col = col
+        }
     }
 
     get(row, col) {
@@ -210,6 +226,9 @@ class HexRoom {
         this.grids["GROUND"].parent = this
         this.grids["TEXT"] = new HexGrid(widthRows, heightCols, EDGE / 2 + RUN, RISE, parity)
         this.grids["TEXT"].parent = this
+        this.grids["ENTITY"] = new HexGrid(widthRows, heightCols, EDGE / 2 + RUN, RISE, parity)
+        this.grids["ENTITY"].parent = this
+        
     }
 
     getCell(x, y) {
@@ -257,14 +276,171 @@ class HexRoom {
         this.grids[layer].set(row, col, val)
     }
 
+    prerender() {
+
+        for (let row = 0; row < this.numRows; row++) {
+            let off = (row + this.parity) % 2
+            for (let col = off; col < this.numCols; col += 2) {
+                let entity = this.grids["ENTITY"].get(row, col)
+                if (entity !== null) {
+                    let text = this.grids["TEXT"].get(row, col)
+                    text.alpha = 0
+                }
+            }
+        }
+    }
+
     render() {
         this.absX = this.x + (this.parent?.absX ?? 0)
         this.absY = this.y + (this.parent?.absY ?? 0)
 
         this.grids["GROUND"].render()
         this.grids["TEXT"].render()
+        this.grids["ENTITY"].render()
     }
 }
+
+// Entity
+class Entity {
+    constructor(sprite, x = 0, y = 0) {
+        this.sprite = sprite
+        sprite.parent = this
+
+        this.x = x
+        this.y = y
+        this.absX = this.x
+        this.absY = this.y
+
+        this.animation = null
+    }
+
+    render() {
+        this.absX = this.x + (this.parent?.absX ?? 0)
+        this.absY = this.y + (this.parent?.absY ?? 0)
+
+        this.sprite.render()
+    }
+}
+
+class TextEntity extends Entity {
+    constructor(char, x = 0, y = 0) {
+        let sprite = new TextSprite(char, 0, 0)
+        super(sprite, x, y)
+        this.row = null
+        this.col = null
+    }
+}
+
+class KeyFrame {
+    constructor(timestamp, update = null) {
+        this.timestamp = timestamp
+        this.update = update
+    }
+
+    execute() {
+        if (this.update !== null) this.update()
+    }
+}
+
+class Tween {
+    constructor(duration, tweenFn) {
+        this.duration = duration
+        this.tweenFn = tweenFn
+        this.elapsedTime = 0
+    }
+
+    update(delta) {
+        this.elapsedTime += delta
+        this.tweenFn((this.elapsedTime > this.duration) ? this.duration : this.elapsedTime)
+
+        return this.duration - this.elapsedTime
+    }
+}
+
+class KeyframeAnimation {
+    constructor(keyframes, tweens) {
+        if (keyframes.length !== tweens.length + 1) {
+            console.log(`Error!  Animation consists of ${keyframes.length} keyframes and ${tweens.length} tweens!`)
+        }
+
+        this.keyframes = keyframes
+        this.tweens = tweens
+        this.elapsedTime = 0
+        this.lastKeyframeIndex = -1
+    }
+
+    start() {
+        let firstKeyframe = this.keyframes[0]
+        if (firstKeyframe.timestamp !== 0) {
+            console.log(`Error!  Animation start() without initial keyframe!`)
+        }
+
+        firstKeyframe.execute()
+
+        this.lastKeyframeIndex = 0
+    }
+
+    update(delta) {
+        this.elapsedTime += delta
+
+        for (let i = this.lastKeyframeIndex + 1; i < this.keyframes.length && this.keyframes[i].timestamp <= this.elapsedTime; i++) {
+            this.keyframes[i].execute()
+            this.lastKeyframeIndex = i
+            delta = this.elapsedTime - this.keyframes[i].timestamp
+        }
+
+        if (this.lastKeyframeIndex < 0 || this.lastKeyframeIndex >= this.tweens.length) {
+            return this.keyframes[this.tweens.length].timestamp - this.elapsedTime
+        }
+
+        this.tweens[this.lastKeyframeIndex].update(delta)
+
+        return this.keyframes[this.tweens.length].timestamp - this.elapsedTime
+    }
+}
+
+class HexMoveAnimation extends KeyframeAnimation {
+    constructor(entity, hexgrid, destRow, destCol, duration) {
+        if (entity.parent !== hexgrid) {
+            console.log("Error! HexMove animation called on entity with incorrect grid!")
+        }
+
+        let startX = entity.x
+        let startY = entity.y
+        let destX = destCol * (EDGE + RUN)
+        let destY = destRow * RISE
+
+        function startFn() {
+            console.log("Beginning move!")
+        }
+
+        function tweenFn(timestamp) {
+            entity.x = (1 - timestamp / duration) * startX + (timestamp / duration) * destX
+            entity.y = (1 - timestamp / duration) * startY + (timestamp / duration) * destY
+        }
+
+        function endFn() {
+            console.log("Move ended!")
+            hexgrid.set(entity.row, entity.col, null)
+            hexgrid.set(destRow, destCol, entity)
+            entity.animation = null
+        }
+
+        let keyframes = [new KeyFrame(0, startFn), new KeyFrame(duration, endFn)]
+        let tweens = [new Tween(duration, tweenFn)]
+
+        super(keyframes, tweens)
+
+        this.entity = entity
+        this.startX = startX
+        this.startY = startY
+        this.destX = destX
+        this.destY = destY
+        this.duration = duration
+    }
+}
+
+// The main scene
 
 function clearCanvas(bkgFill = null) {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -278,50 +454,46 @@ let parity = 1
 
 let hexroom = new HexRoom(11, 7, 200, 200, parity)
 
-// hexgrid = new HexGrid(11, 19, 200, 200)
+function makeHexRoom(hexroom) {
+    // hexgrid = new HexGrid(11, 19, 200, 200)
 
-for (let row = 0; row < hexroom.numRows; row++) {
-    let off = (row + parity) % 2
-    for (let col = off; col < hexroom.numCols; col += 2) {
-        let hex = new Hexagon()
-        hexroom.set("GROUND", row, col, hex)
+    for (let row = 0; row < hexroom.numRows; row++) {
+        let off = (row + parity) % 2
+        for (let col = off; col < hexroom.numCols; col += 2) {
+            let hex = new Hexagon()
+            hexroom.set("GROUND", row, col, hex)
+        }
+    }
+
+    // textgrid = new HexGrid(11, 19, 200, 200)
+
+    for (let row = 0; row < hexroom.numRows; row++) {
+        let off = (row + parity) % 2
+        for (let col = off; col < hexroom.numCols; col += 2) {
+            let hex = new TextSprite("\"")
+            hexroom.set("TEXT", row, col, hex)
+        }
+    }
+
+    for(let col = 0; col < hexroom.numCols; col++) {
+        hexroom.set("ENTITY", (col + parity) % 2, col, new TextSprite("█"))
+        hexroom.set("ENTITY", hexroom.numRows - 1 - ((col + parity) % 2), col, new TextSprite("█"))
+    }
+
+    for(let row = parity; row < hexroom.numRows; row += 2) {
+        hexroom.set("ENTITY", row, 0, new TextSprite("█"))
+        hexroom.set("ENTITY", row, hexroom.numCols - 1, new TextSprite("█"))
     }
 }
 
-// textgrid = new HexGrid(11, 19, 200, 200)
-
-for (let row = 0; row < hexroom.numRows; row++) {
-    let off = (row + parity) % 2
-    for (let col = off; col < hexroom.numCols; col += 2) {
-        let hex = new TextSprite("\"")
-        hexroom.set("TEXT", row, col, hex)
-    }
-}
-
-for(let col = 0; col < hexroom.numCols; col++) {
-    hexroom.set("TEXT", (col + parity) % 2, col, new TextSprite("█"))
-    
-    hexroom.set("TEXT", hexroom.numRows - 1 - ((col + parity) % 2), col, new TextSprite("█"))
-}
-
-for(let row = parity; row < hexroom.numRows; row += 2) {
-    hexroom.set("TEXT", row, 0, new TextSprite("█"))
-    hexroom.set("TEXT", row, hexroom.numCols - 1, new TextSprite("█"))
-}
-
-
-
-hexroom.set("TEXT", 4, 2 + parity, new TextSprite("@", 0, 0))
-hexroom.set("TEXT", 6, 2 + parity, new TextSprite("我", 0, 4 * RISE))
+let player = new TextEntity("@", 0, 0)
 
 let loc = [null, null]
 
-function renderFrame(timestamp) {
+function renderFrame() {
     clearCanvas("#282828")
 
-    hexroom.x = Math.floor((canvas.width - hexroom.pxWidth) / 2)
-    hexroom.y = Math.floor((canvas.height - hexroom.pxHeight) / 2)
-
+    hexroom.prerender()
     hexroom.render()
     ctx.strokeRect(hexroom.x, hexroom.y, hexroom.pxWidth, hexroom.pxHeight)
     drawDots(0, 0, 50)
@@ -331,6 +503,19 @@ function renderFrame(timestamp) {
         ctx.beginPath()
         ctx.arc(mouseLoc.x, mouseLoc.y, 5, 0, 2 * Math.PI, false)
         ctx.fill()
+    }
+}
+
+function updateLoop(timestamp) {
+
+    prevTime = currTime
+    currTime = timestamp
+    let delta = (currTime - prevTime) / 1000
+
+    hexroom.x = Math.floor((canvas.width - hexroom.pxWidth) / 2)
+    hexroom.y = Math.floor((canvas.height - hexroom.pxHeight) / 2)
+
+    if (mouseLoc.x !== null && mouseLoc.y !== null) {
 
         let oldloc = loc
         loc = hexroom.getCell(mouseLoc.x, mouseLoc.y)
@@ -347,11 +532,58 @@ function renderFrame(timestamp) {
         }
     }
 
-    requestAnimationFrame(renderFrame)
+    player.animation.update(delta)
+
+    function isEmpty(row, col) {
+        if (0 > row || row >= hexroom.numRows) return false
+        if (0 > col || col >= hexroom.numCols) return false
+        return (hexroom.get("ENTITY", row, col) === null)
+    }
+
+    if (player.animation === null) {
+        let opencells = []
+        if (isEmpty(player.row + 2, player.col)) opencells.push([player.row + 2, player.col])
+        if (isEmpty(player.row + 1, player.col + 1)) opencells.push([player.row + 1, player.col + 1])
+        if (isEmpty(player.row - 1, player.col + 1)) opencells.push([player.row - 1, player.col + 1])
+        if (isEmpty(player.row - 2, player.col)) opencells.push([player.row - 2, player.col])
+        if (isEmpty(player.row - 1, player.col - 1)) opencells.push([player.row - 1, player.col - 1])
+        if (isEmpty(player.row + 1, player.col - 1)) opencells.push([player.row + 1, player.col - 1])
+
+        dest = opencells[Math.floor(Math.random() * opencells.length)]
+
+        let anim = new HexMoveAnimation(player, hexroom.grids["ENTITY"], dest[0], dest[1], 1.0)
+        player.animation = anim
+        player.animation.start()
+    }
+
+    renderFrame()
+
+    requestAnimationFrame(updateLoop)
+}
+
+function initialize(timestamp) {
+    prevTime = currTime
+    currTime = timestamp
+
+    makeHexRoom(hexroom)
+
+    hexroom.x = Math.floor((canvas.width - hexroom.pxWidth) / 2)
+    hexroom.y = Math.floor((canvas.height - hexroom.pxHeight) / 2)
+
+    hexroom.set("ENTITY", 4, 2 + parity, player)
+    hexroom.set("ENTITY", 6, 2 + parity, new TextEntity("零", 0, 4 * RISE))
+
+    let anim = new HexMoveAnimation(player, hexroom.grids["ENTITY"], 5, 3 + parity, 1.0)
+    player.animation = anim
+    player.animation.start()
+    
+    renderFrame()
+
+    requestAnimationFrame(updateLoop)
 }
 
 // hexgrid.render()
 // textgrid.render()
 
 
-requestAnimationFrame(renderFrame)
+requestAnimationFrame(initialize)
